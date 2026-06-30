dd670abf5b6c051511224593cf058b5d1ac402cb
AndreFirmansyah06 <indomieseleramu1@gmail.com>
2026-06-30 16:51:57 +0700

    feat:insialisasi model KnowledgeRepository di class KnowledgeController

--- src/main/java/com/example/tugasbesarpbo/controller/KnowledgeController.java
+++ src/main/java/com/example/tugasbesarpbo/controller/KnowledgeController.java
@@ -1,4 +1,14 @@
 package com.example.tugasbesarpbo.controller;
 
+import  com.example.tugasbesarpbo.util.InputHandler;
+import com.example.tugasbesarpbo.model.KnowledgeRepository;
+import java.util.ArrayList;
+
 public class KnowledgeController {
+    private final KnowledgeRepository repository;
+
+    public KnowledgeController(KnowledgeRepository repository) {
+
+        this.repository = repository;
+    }
 }
