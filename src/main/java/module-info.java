763e4b286293eb88ada0672b040c6df3d6d17bc6
AndreFirmansyah06 <indomieseleramu1@gmail.com>
2026-07-01 08:14:34 +0700

    feat:menambahkan method untuk fitur import file csv

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tugasbesarpbo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.csv;
 
 
     opens com.example.tugasbesarpbo to javafx.fxml;
